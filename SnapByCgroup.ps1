42e36b2f19e946b5d69bfee35aaf20ecb07cf480
Thomas H Jones II <gitlab@xanthia.com>
2015-04-16 11:20:00 -0400

    Initial check-in * Create header-block

---
+++ SnapByCgroup.ps1
@@ -0,0 +1,17 @@
+# This script is designed to perform consistent backups of
+# - Selected EBS volumes (referenced by "Consistency Group"
+#   AWS object-tag)
+# - Mounted filesystems (referenced by Windows drive-letter
+#   or fully-qualified directory-path)
+#
+#
+# Dependencies:
+# - Generic: See the top-level README_dependencies.md file for
+#   a full list of script dependencies
+#
+# License:
+# - This script released under the Apache 2.0 OSS License
+#
+######################################################################
+
+
